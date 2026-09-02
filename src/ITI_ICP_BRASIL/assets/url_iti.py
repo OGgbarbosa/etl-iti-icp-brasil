f5df8b3c702c1c8683b07735d2abc9c922ecd127
Gabriel Barbosa <gbarbos4@gmail.com>
2026-09-02 20:54:13 -0300

    ADD: URL acesso

---
+++ src/ITI_ICP_BRASIL/assets/url_iti.py
@@ -0,0 +1,12 @@
+import requests
+from ITI_ICP_BRASIL.processamento.flatten import flatten
+
+url = 'https://www.estrutura.iti.gov.br/assets/json/details.json'
+
+def obter_entidade():
+    response = requests.get(url, timeout=30)
+    response.raise_for_status()
+
+    dados = response.json()
+
+    return [flatten(item) for item in dados['entidades']]
