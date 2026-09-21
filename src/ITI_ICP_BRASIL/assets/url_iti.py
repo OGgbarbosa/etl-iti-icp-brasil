47f361d5fe4042bf45c562bdadf8e839414db3d6
Gabriel Barbosa <gbarbos4@gmail.com>
2026-09-21 02:04:37 -0300

    feat(assets): adiciona obter_dados_num e integra URLs a partir de config

--- src/ITI_ICP_BRASIL/assets/url_iti.py
+++ src/ITI_ICP_BRASIL/assets/url_iti.py
@@ -1,13 +1,20 @@
 import requests
 
-from ITI_ICP_BRASIL.processamento.flatten import flatten
+from ITI_ICP_BRASIL.config.config import URL_ENTIDADES, URL_NUMEROS
+from ITI_ICP_BRASIL.processamento.flatten import flatten, flatten_num
 
-url = 'https://estrutura.iti.gov.br/assets/jsons/details.json'
 
 def obter_entidade():
-    response = requests.get(url, timeout=30)
+    response = requests.get(URL_ENTIDADES, timeout=30)
     response.raise_for_status()
 
     dados = response.json()
 
     return [flatten(item) for item in dados['entidades']]
+
+def obter_dados_num():
+    response = requests.get(URL_NUMEROS, timeout=30)
+    response.raise_for_status()
+    dados = response.json()
+
+    return flatten_num(dados)
