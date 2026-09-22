44146cc0a99d8865e14d15e0778fdd9da86a484d
Gabriel Barbosa <gbarbos4@gmail.com>
2026-09-22 02:09:58 -0300

    feat(bronze): adiciona caminhos no config, refatora upload_bronze com logger e suporte a entidades e numeros

--- src/ITI_ICP_BRASIL/config/config.py
+++ src/ITI_ICP_BRASIL/config/config.py
@@ -7,6 +7,16 @@ volume_raw_entidades = "/Volumes/lakehouse_iti/0_raw/raw/entidades.json"
 volume_raw_numeros = "/Volumes/lakehouse_iti/0_raw/raw/numeros.json"
 
 # Bronze
+caminho_raw_entidades = "/Volumes/lakehouse_iti/0_raw/raw/entidades.json"
+caminho_raw_numeros = "/Volumes/lakehouse_iti/0_raw/raw/numeros.json"
+
+caminho_bronze_entidades = "/Volumes/lakehouse_iti/1_bronze/raw/entidades.csv"
+caminho_bronze_numeros = "/Volumes/lakehouse_iti/1_bronze/raw/numeros.csv"
+
+nome_volume_raw = "lakehouse_iti.1_bronze.raw"
+
+tabela_destino_entidades = "lakehouse_iti.1_bronze.entidades"
+tabela_destino_numeros = "lakehouse_iti.1_bronze.numeros"
 
 # Silver
 
