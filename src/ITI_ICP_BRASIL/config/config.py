5c0a0bd8aa74dc8c2e1b85509386aab95a9463e3
Gabriel Barbosa <gbarbos4@gmail.com>
2026-09-21 21:16:04 -0300

    feat(raw_numeros): adicionando variaveis de ambiente para camada raw

--- src/ITI_ICP_BRASIL/config/config.py
+++ src/ITI_ICP_BRASIL/config/config.py
@@ -1,2 +1,13 @@
+# Assets
 URL_ENTIDADES = "https://estrutura.iti.gov.br/assets/jsons/details.json"
-URL_NUMEROS = "https://numeros.iti.gov.br/assets/paneljson/panels.json"
+URL_NUMEROS = "https://numeros.iti.gov.br/assets/paneljson/panels.json"
+
+# Raw
+volume_raw_entidades = "/Volumes/lakehouse_iti/0_raw/raw/entidades.json"
+volume_raw_numeros = "/Volumes/lakehouse_iti/0_raw/raw/numeros.json"
+
+# Bronze
+
+# Silver
+
+# Gold
