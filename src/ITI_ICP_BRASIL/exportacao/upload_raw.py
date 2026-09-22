fee022a91b2c0f9a1e4d7ece8d51acc02f345b2f
Gabriel Barbosa <gbarbos4@gmail.com>
2026-09-21 21:17:00 -0300

    feat(raw_numeros): adicionando controle de logger

--- src/ITI_ICP_BRASIL/exportacao/upload_raw.py
+++ src/ITI_ICP_BRASIL/exportacao/upload_raw.py
@@ -5,10 +5,15 @@ import logging
 from databricks.sdk import WorkspaceClient
 
 from ITI_ICP_BRASIL.assets.url_iti import obter_dados_num, obter_entidade
+from ITI_ICP_BRASIL.config.config import (
+    volume_raw_entidades,
+    volume_raw_numeros,
+)
 
 logger = logging.getLogger(__name__)
 
-def upload_para_volume():
+
+def upload_volume_iti_entidades():
     # Inicializa o cliente do Databricks usando a autenticação configurada
     try:
         w = WorkspaceClient()
@@ -17,7 +22,7 @@ def upload_para_volume():
         raise
 
     # Caminho do Volume no Unity Catalog
-    caminho_volume = "/Volumes/lakehouse_iti/0_raw/raw/entidades.json"
+    caminho_volume_iti_entidades = volume_raw_entidades
 
     logger.info("🌐 Obtendo dados da API do ITI...")
     try:
@@ -26,8 +31,8 @@ def upload_para_volume():
         logger.error("🟥 Erro ao obter dados da API: %s", e)
         raise
 
-    print(f"Total de entidades obtidas: {len(dados)}")
-    print(f"Fazendo upload para o Volume Databricks: {caminho_volume}")
+    logger.info("Total de registros obtidos: %d.", len(dados))
+    logger.info("Fazendo upload para o Volume Databricks: %s", caminho_volume_iti_entidades)
 
     # Converte o JSON em bytes
     try:
@@ -39,7 +44,7 @@ def upload_para_volume():
     # Faz o upload diretamente para o Volume via API
     try:
         w.files.upload(
-            file_path=caminho_volume,
+            file_path=caminho_volume_iti_entidades,
             contents=io.BytesIO(conteudo_bytes),
             overwrite=True
         )
@@ -56,9 +61,9 @@ def upload_volume_iti_numeros():
         logger.error(f"🟥 Erro ao conectar ao Databricks: {e}")
         raise
 
-    caminho_volume_numeros = "/Volumes/lakehouse_iti/0_raw/raw/numeros.json"
+    caminho_volume_iti_numeros = volume_raw_numeros
 
-    print("🌐 Obtendo dados da API do ITI")
+    logger.info("🌐 Obtendo dados da API do ITI...")
     try:
         dados = obter_dados_num()
     except Exception as e:
@@ -66,7 +71,7 @@ def upload_volume_iti_numeros():
         raise
 
     logger.info("Total de registros obtidos: %d.", len(dados))
-    logger.info("Fazendo upload para o Volume Databricks: %s", caminho_volume_numeros)
+    logger.info("Fazendo upload para o Volume Databricks: %s", caminho_volume_iti_numeros)
     try:
         conteudo_bytes = json.dumps(dados, ensure_ascii=False, indent=2).encode("utf-8")
     except Exception as e:
@@ -75,7 +80,7 @@ def upload_volume_iti_numeros():
 
     try:
         w.files.upload(
-            file_path=caminho_volume_numeros,
+            file_path=caminho_volume_iti_numeros,
             contents=io.BytesIO(conteudo_bytes),
             overwrite=True
         )
@@ -84,3 +89,6 @@ def upload_volume_iti_numeros():
         logger.error(f"🟥 Erro ao fazer upload para o Volume: {e}")
         raise
     
+if __name__ == "__main__":
+    upload_volume_iti_entidades ()
+    upload_volume_iti_numeros()
